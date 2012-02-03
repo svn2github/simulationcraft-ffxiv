svn2github/simulationcraft-ffxiv

commit 2950bc948f04c46f9ec5321e116cad220d2d3cee

Paladin:   Ancient Fury uses spell power, not attack
git-svn-id: http://simulationcraft.googlecode.com/svn/trunk@10977 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_paladin.cpp
@@ -1171,10 +1171,10 @@ static void trigger_tier12_2pc_melee( attack_t* s, double dmg )
 
 // Ancient Fury =============================================================
 
-struct ancient_fury_t : public paladin_attack_t
+struct ancient_fury_t : public paladin_spell_t
 {
-  ancient_fury_t( paladin_t* p )
-    : paladin_attack_t( "ancient_fury", 86704, p, false )
+  ancient_fury_t( paladin_t* p ) :
+    paladin_spell_t( "ancient_fury", 86704, p )
   {
     // TODO meteor stuff
     background = true;
@@ -1183,13 +1183,13 @@ struct ancient_fury_t : public paladin_attack_t
 
   virtual void execute()
   {
-    paladin_attack_t::execute();
+    paladin_spell_t::execute();
     player -> cast_paladin() -> buffs_ancient_power -> expire();
   }
 
   virtual void player_buff()
   {
-    paladin_attack_t::player_buff();
+    paladin_spell_t::player_buff();
     player_multiplier *= player -> cast_paladin() -> buffs_ancient_power -> stack();
   }
 };

commit 39224125ff3720f36de559683ea8bd0c6b5658f1

Paladin:   Flag Ancient Fury as may_crit again, broken in r10977
git-svn-id: http://simulationcraft.googlecode.com/svn/trunk@10981 3b4652a1-8050-0410-ac47-3d40261b0f8b

engine/sc_paladin.cpp
@@ -1179,6 +1179,7 @@ struct ancient_fury_t : public paladin_spell_t
     // TODO meteor stuff
     background = true;
     callbacks  = false;
+    may_crit   = true;
   }
 
   virtual void execute()